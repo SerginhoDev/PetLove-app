3a3e6d42caa3e54ce1f4b16cc9d8c9aaac859856
Sergey Sirota <seregasirota338@gmail.com>
2026-04-28 21:55:02 +0200

    chore: update vite config and tsconfig aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 
 	resolve: {
-		tsconfigPaths: true,
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@assets": path.resolve(__dirname, "./src/assets"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@shared": path.resolve(__dirname, "./src/components/shared"),
+			"@pages": path.resolve(__dirname, "./src/pages"),
+			"@styles": path.resolve(__dirname, "./src/styles"),
+		},
 	},
 
 	css: {
